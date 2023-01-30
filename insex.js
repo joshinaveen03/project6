96cb8744269891a314a726463e8eb07834b3ff68
joshinaveen03 <joshi.naveeen03@gmail.com>
2023-01-30 12:28:53 +0530

    project6

---
+++ insex.js
@@ -0,0 +1,227 @@
+let a =[[
+    {
+      "_id": "63d764f09a7377e0b8d4d259",
+      "index": 0,
+      "guid": "7c78125e-5384-4c39-8f29-991c1ed7df17",
+      "isActive": true,
+      "balance": "$1,132.70",
+      "picture": "http://placehold.it/32x32",
+      "age": 36,
+      "eyeColor": "green",
+      "name": "Carney Dickerson",
+      "gender": "male",
+      "company": "ROCKYARD",
+      "email": "carneydickerson@rockyard.com",
+      "phone": "+1 (965) 463-3056",
+      "address": "835 Terrace Place, Faxon, Rhode Island, 8352",
+      "about": "Laborum non elit ad sint irure. Do enim occaecat anim reprehenderit ad ad aliquip proident ipsum. Aliquip dolor incididunt id excepteur ut velit. Aliquip mollit magna excepteur ad. Irure quis mollit tempor officia aliqua incididunt ut nisi sunt qui.\r\n",
+      "registered": "2022-07-18T01:05:08 -06:-30",
+      "latitude": -27.336636,
+      "longitude": -103.056324,
+      "tags": [
+        "cillum",
+        "culpa",
+        "ipsum",
+        "aliquip",
+        "consequat",
+        "eiusmod",
+        "dolore"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Duncan Warren"
+        },
+        {
+          "id": 1,
+          "name": "Jasmine Pierce"
+        },
+        {
+          "id": 2,
+          "name": "Hewitt Wagner"
+        }
+      ],
+      "greeting": "Hello, Carney Dickerson! You have 6 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "63d764f01ac84861d2cbe6c9",
+      "index": 1,
+      "guid": "e2a26960-481e-4afc-91e1-1c45bb11c875",
+      "isActive": false,
+      "balance": "$3,216.19",
+      "picture": "http://placehold.it/32x32",
+      "age": 23,
+      "eyeColor": "green",
+      "name": "Grimes Hickman",
+      "gender": "male",
+      "company": "ZIORE",
+      "email": "grimeshickman@ziore.com",
+      "phone": "+1 (872) 503-2523",
+      "address": "912 Russell Street, Martinez, Iowa, 682",
+      "about": "Anim incididunt eiusmod exercitation labore ex. Pariatur exercitation sunt sunt velit amet occaecat proident occaecat culpa qui nulla tempor ipsum. Proident commodo sint duis ut ut est.\r\n",
+      "registered": "2016-05-18T07:11:07 -06:-30",
+      "latitude": 73.875009,
+      "longitude": -108.400873,
+      "tags": [
+        "quis",
+        "esse",
+        "Lorem",
+        "culpa",
+        "sint",
+        "deserunt",
+        "duis"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Hawkins Snider"
+        },
+        {
+          "id": 1,
+          "name": "Suarez Justice"
+        },
+        {
+          "id": 2,
+          "name": "Gallegos Hensley"
+        }
+      ],
+      "greeting": "Hello, Grimes Hickman! You have 9 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "63d764f024329972217e3154",
+      "index": 2,
+      "guid": "532d9986-6b60-4d17-9750-1a49db52600e",
+      "isActive": true,
+      "balance": "$3,350.51",
+      "picture": "http://placehold.it/32x32",
+      "age": 21,
+      "eyeColor": "green",
+      "name": "Hinton Hughes",
+      "gender": "male",
+      "company": "GEEKNET",
+      "email": "hintonhughes@geeknet.com",
+      "phone": "+1 (933) 453-3652",
+      "address": "646 Dupont Street, Wells, Hawaii, 5674",
+      "about": "Id pariatur labore occaecat do ea. Cupidatat exercitation magna cillum esse mollit amet quis cillum. Cillum eu sint dolor anim amet irure esse. Eiusmod Lorem fugiat Lorem excepteur dolore aute. Nostrud voluptate excepteur tempor velit esse et labore cillum consequat irure adipisicing.\r\n",
+      "registered": "2016-08-28T05:57:44 -06:-30",
+      "latitude": 55.262894,
+      "longitude": -68.818412,
+      "tags": [
+        "eu",
+        "voluptate",
+        "est",
+        "non",
+        "cupidatat",
+        "ullamco",
+        "tempor"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Hahn Baker"
+        },
+        {
+          "id": 1,
+          "name": "Kellie Brennan"
+        },
+        {
+          "id": 2,
+          "name": "Rachel Madden"
+        }
+      ],
+      "greeting": "Hello, Hinton Hughes! You have 5 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "63d764f0ba1eb11a460741a8",
+      "index": 3,
+      "guid": "de55ddf5-5d0d-42a0-80bc-c53a701f6dc1",
+      "isActive": false,
+      "balance": "$2,056.97",
+      "picture": "http://placehold.it/32x32",
+      "age": 39,
+      "eyeColor": "green",
+      "name": "Stout Middleton",
+      "gender": "male",
+      "company": "COLAIRE",
+      "email": "stoutmiddleton@colaire.com",
+      "phone": "+1 (910) 442-3594",
+      "address": "944 Bassett Avenue, Wanamie, Florida, 7645",
+      "about": "Anim et anim amet dolor quis minim. Amet aute exercitation veniam veniam anim veniam exercitation. Ad elit eiusmod nisi sit et incididunt pariatur non sint tempor cupidatat. Officia labore officia consectetur nostrud excepteur incididunt elit reprehenderit officia sit.\r\n",
+      "registered": "2022-07-26T03:29:30 -06:-30",
+      "latitude": 78.333013,
+      "longitude": -82.042408,
+      "tags": [
+        "mollit",
+        "et",
+        "occaecat",
+        "ipsum",
+        "voluptate",
+        "mollit",
+        "commodo"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Vicki Donaldson"
+        },
+        {
+          "id": 1,
+          "name": "Patti Kemp"
+        },
+        {
+          "id": 2,
+          "name": "Mcpherson Gregory"
+        }
+      ],
+      "greeting": "Hello, Stout Middleton! You have 2 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "63d764f0b1a7fe9a486a6e22",
+      "index": 4,
+      "guid": "b98b7acd-651e-4a1b-a347-ef75c5bc319c",
+      "isActive": true,
+      "balance": "$3,775.03",
+      "picture": "http://placehold.it/32x32",
+      "age": 25,
+      "eyeColor": "green",
+      "name": "Tillman Mayo",
+      "gender": "male",
+      "company": "HALAP",
+      "email": "tillmanmayo@halap.com",
+      "phone": "+1 (942) 558-3185",
+      "address": "714 Turner Place, Nash, New Hampshire, 2390",
+      "about": "Occaecat qui consequat dolore officia qui ullamco culpa in eiusmod laboris enim. Occaecat minim officia et magna proident aliquip pariatur aliqua. Fugiat veniam aute id voluptate. Officia consequat sunt mollit aliqua anim culpa ea nulla quis est sunt nulla. Minim pariatur veniam dolor nostrud.\r\n",
+      "registered": "2018-03-18T08:12:45 -06:-30",
+      "latitude": 63.816471,
+      "longitude": 138.153647,
+      "tags": [
+        "eiusmod",
+        "esse",
+        "proident",
+        "reprehenderit",
+        "exercitation",
+        "officia",
+        "amet"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Darlene Ellison"
+        },
+        {
+          "id": 1,
+          "name": "Pansy Norton"
+        },
+        {
+          "id": 2,
+          "name": "Michael Horne"
+        }
+      ],
+      "greeting": "Hello, Tillman Mayo! You have 9 unread messages.",
+      "favoriteFruit": "apple"
+    }
+  ]]
